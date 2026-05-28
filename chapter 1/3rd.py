36c416868619513c77ec3a3d62d51f6e4143dbc9
nikhal <nikhaldaroch47@gmail.com>
2026-05-28 11:23:22 +0530

    new data added liberaies and datasets

--- chapter 1/3rd.py
+++
@@ -1,5 +0,0 @@
-#a = "python"
-#print(a[::-1])
-a = "python"
-for i in range(len(a)-1,-1,-1):
-    print(a[i],end=" ")
--- chapter 1/5th.py
+++
@@ -1,7 +0,0 @@
-a = 0
-b = 1
-print(a,b,end=" ")
-for i in range(8):
-    next = a+b
-    print(next,end=" ")
-    a,b=b,next 
